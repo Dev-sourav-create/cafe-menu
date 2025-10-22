9147fb8c6adf2849cf3d7d330b5a060413760f09
Ankur Prajapati <ankurbit1997@gmail.com>
2025-10-22 23:23:15 +0530

    fix

--- src/Components/BentroGrid.jsx
+++ src/Components/BentroGrid.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/Components/ui/Card";
 import { Button } from "@/Components/ui/Button";
 import {
   QrCode,
