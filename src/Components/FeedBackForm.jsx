a80ea24896e29fd53071d84800a6e107bec32d87
Ankur Prajapati <ankurbit1997@gmail.com>
2025-10-22 23:02:27 +0530

    fix

--- src/Components/FeedBackForm.jsx
+++ src/Components/FeedBackForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
-import { db } from "@/firbase";
+import { db } from "@/firebase";
 import { addDoc, collection, doc, getDoc } from "firebase/firestore";
 import { StarRating } from "./StarRating";
 import toast from "react-hot-toast";
